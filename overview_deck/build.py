2ee4d8ac3b6ce34bee87a747fea9d5692f82f5df
John Mertic <jmertic@linuxfoundation.org>
2024-03-11 17:16:15 -0400

    Move to building and adding all overview decks to foundation repo
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- overview_deck/build.py
+++ overview_deck/build.py
@@ -13,6 +13,8 @@ import requests
 documents = [
         {'id':'1NKy7Cdfh60YgDQMQxc3pB5UNM5SUd2kh','mimeType':'application/pdf','filename':'ASWF High Level Overview.pdf'},
         {'id':'1NKy7Cdfh60YgDQMQxc3pB5UNM5SUd2kh','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'ASWF High Level Overview.pptx'},
+        {'id':'1DymqK796EhxkLIchx7sNxJloovYLGJD8UDkOdkS42to','mimeType':'application/pdf','filename':'ASWF Governing Board Overview.pdf'},
+        {'id':'1qY9DPLw4aucqIdqqB1_Pi-qKEyIg2V7q9B96-G1jhqE','mimeType':'application/pdf','filename':'ASWF TAC Overview.pdf'},
         {'id':'1p0FoFJ7-IdDejisJPUYdq_uApn3XW3wJ','mimeType':'application/pdf','filename':'ASWF Membership Overview.pdf'},
         ]
 
